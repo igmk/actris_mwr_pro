daae5097ce827c4e4aa261676ce27a0c3c5ad91c
tobiasmarke <tmarke@meteo.uni-koeln.de>
2021-12-03 16:04:57 +0100

    added structure for Level 2

---
+++ rpg_mwr.py
@@ -0,0 +1,204 @@
+from typing import Optional, Union, Tuple
+import numpy as np
+import numpy.ma as ma
+import netCDF4
+from level1.lev1_meta_nc import MetaData
+import utils
+
+class RpgArray:
+    """Stores netCDF4 variables, numpy arrays and scalars as RpgArrays.
+    Args:
+        variable: The netCDF4 :class:`Variable` instance, numpy array (masked or regular),
+            or scalar (float, int).
+        name: Name of the variable.
+        units_from_user: Units of the variable.
+    Attributes:
+        name (str): Name of the variable.
+        data (ndarray): The actual data.
+        data_type (str): 'i4' for integers, 'f4' for floats.
+        units (str): The `units_from_user` argument if it is given. Otherwise
+            copied from the original netcdf4 variable. Empty if input is just data.
+    """
+
+    def __init__(self,
+                 variable: Union[netCDF4.Variable, np.ndarray, float, int],
+                 name: str,
+                 units_from_user: Optional[str] = None,
+                 dimensions: Optional[tuple] = None):
+        self.variable = variable
+        self.name = name
+        self.data = self._init_data()
+        self.units = self._init_units(units_from_user)
+        self.data_type = self._init_data_type()
+        self.dimensions = dimensions
+
+    def fetch_attributes(self) -> list:
+        """Returns list of user-defined attributes."""
+        
+        attributes = []
+        for attr in self.__dict__:
+            if attr not in ('name', 'data', 'data_type', 'variable', 'dimensions'):
+                attributes.append(attr)
+        return attributes
+
+    def set_attributes(self, 
+                       attributes: MetaData) -> None:
+        """Overwrites existing instance attributes."""
+        
+        for key in attributes._fields:  # To iterate namedtuple fields.
+            data = getattr(attributes, key)
+            if data:
+                setattr(self, key, data)
+
+    def _init_data(self) -> np.ndarray:
+        if isinstance(self.variable, netCDF4.Variable):
+            return self.variable[:]
+        if isinstance(self.variable, np.ndarray):
+            return self.variable
+        if isinstance(self.variable, (int, float)):
+            return np.array(self.variable)
+        if isinstance(self.variable, str):
+            try:
+                numeric_value = utils.str_to_numeric(self.variable)
+                return np.array(numeric_value)
+            except ValueError:
+                pass
+        raise ValueError(f'Incorrect RpgArray input: {self.variable}')
+
+    def _init_units(self, 
+                    units_from_user: Union[str, None]) -> str:
+        if units_from_user is not None:
+            return units_from_user
+        return getattr(self.variable, 'units', '')
+
+    def _init_data_type(self) -> str:
+        if self.data.dtype in (np.float32, np.float64):
+            return 'f4'
+        return 'i4'
+
+    def __getitem__(self, ind: tuple) -> np.ndarray:
+        return self.data[ind]
+
+
+class Rpg:
+    """Base class for RPG MWR."""
+    
+    def __init__(self, raw_data: dict):
+        self.raw_data = raw_data
+        self.data = {}
+        self.data = self._init_data()
+
+    def _init_data(self) -> dict:
+        data = {}
+        for key in self.raw_data:
+            data[key] = RpgArray(self.raw_data[key], key)
+        return data
+    
+    
+def save_rpg(rpg: Rpg,
+             output_file: str,
+             att: dict,
+             data_type: str,
+             params: dict) -> Tuple[str, list]:
+    """Saves the RPG MWR file."""
+    
+    if data_type == '1B01':
+        dims = {'time': len(rpg.data['time'][:]),
+                'frequency': len(rpg.data['tb'][:].T),
+                'n_receivers': len(rpg.data['t_rec'][:].T),
+                'n_sidebands': params['sideband_count'],
+                'time_bnds': 2}
+    elif data_type == '1B11':
+        dims = {'time': len(rpg.data['time'][:]),
+                'ir_wavelength': len(rpg.data['irt'][:].T)}
+    elif data_type == '1B21':
+        dims = {'time': len(rpg.data['time'][:])}        
+    elif data_type == '1C01':
+        dims = {'time': len(rpg.data['time'][:]),
+                'frequency': len(rpg.data['tb'][:].T),
+                'n_receivers': len(rpg.data['t_rec'][:].T),
+                'ir_wavelength': len(rpg.data['irt'][:].T),
+                'n_sidebands': params['sideband_count'],
+                'time_bnds': 2}
+    elif data_type == '2P00':
+        dims = {'time': len(rpg.data['time'][:]),
+               'time_bnds': 2,
+               'altitude' : len(rpg.data['altitude'][:])}
+    elif data_type == '2I00':
+        dims = {'time': len(rpg.data['time'][:]),
+               'time_bnds': 2}
+    elif data_type == '2S00':
+        dims = {'time': len(rpg.data['time'][:]),
+               'time_bnds': 2,
+               'n_freq_spectrum' : len(rpg.data['spectrum'][:])}
+    else:
+        raise RuntimeError(['Data type '+ data_type +' not supported for file writing.'])
+
+    rootgrp = init_file(output_file, dims, rpg.data, att)
+    rootgrp.close()
+    
+    
+def init_file(file_name: str,
+              dimensions: dict,
+              rpg_arrays: dict,
+              att_global: dict) -> netCDF4.Dataset:
+    """Initializes a RPG MWR file for writing.
+    Args:
+        file_name: File name to be generated.
+        dimensions: Dictionary containing dimension for this file.
+        rpg_arrays: Dictionary containing :class:`RpgArray` instances.
+        att_global: Dictionary containing site specific global attributes
+        
+    """
+    nc = netCDF4.Dataset(file_name, 'w', format='NETCDF4_CLASSIC')
+    for key, dimension in dimensions.items():
+        nc.createDimension(key, dimension)
+    _write_vars2nc(nc, rpg_arrays)
+    _add_standard_global_attributes(nc, att_global)
+    return nc
+
+    
+def _get_dimensions(nc: netCDF4.Dataset, 
+                    data: np.ndarray) -> tuple:
+    """Finds correct dimensions for a variable."""
+    
+    if utils.isscalar(data):
+        return ()
+    variable_size = ()
+    file_dims = nc.dimensions
+    array_dims = data.shape
+    for length in array_dims:
+        dim = [key for key in file_dims.keys() if file_dims[key].size == length][0]
+        variable_size = variable_size + (dim,)
+    return variable_size
+
+
+def _write_vars2nc(nc: netCDF4.Dataset, 
+                   cloudnet_variables: dict) -> None:
+    """Iterates over RPG instances and write to netCDF file."""
+    
+    for obj in cloudnet_variables.values():
+        if obj.data_type == 'f4':
+            fill_value = -999.
+        else:
+            fill_value = -99
+            
+        # if ma.isMaskedArray(obj.data):
+        #     fill_value = netCDF4.default_fillvals[obj.data_type]
+        # else:
+        #     fill_value = False
+
+        size = obj.dimensions or _get_dimensions(nc, obj.data)
+        if obj.name == 'time_bounds':
+            size = ('time', 'time_bnds')
+        nc_variable = nc.createVariable(obj.name, obj.data_type, size, zlib=True,
+                                        fill_value=fill_value)
+        nc_variable[:] = obj.data
+        for attr in obj.fetch_attributes():
+            setattr(nc_variable, attr, getattr(obj, attr))
+            
+
+def _add_standard_global_attributes(nc: netCDF4.Dataset, 
+                                    att_global) -> None:
+    for name, value in att_global.items():
+        setattr(nc, name, value)
---
+++ site_config.py
@@ -0,0 +1,205 @@
+from global_nc import GLOBAL_ALL
+import numpy as np
+
+def get_site_specs(site: str, 
+                   data_type: str) -> dict: 
+    """ This function initializes site specific global attributes and parameters of RPG MWR Level 1 data for NetCDF file writing.
+    Args:
+        site: Name of site.
+        data_type: Data type of the netCDF file.
+        
+    Returns:
+        Dictionary
+      
+    Raises:
+        RuntimeError: Specified site or data type is not supported.
+    
+    Example:
+        from level1.site_config import get_global_attributes
+        att, param = get_site_specs('site_name','data_type')
+       
+    """
+
+    specs, params = site_specs(site, data_type)
+    add_global_description(specs, GLOBAL_ALL)
+    
+    return specs, params
+            
+
+def add_global_description(site_specs: dict, 
+                           global_description: dict) -> None:
+    """Adds global attribute description.
+    Args:
+        site_specs: Site specific global attributes.
+        attributes: Global attribute description.
+    """
+    
+    for key in site_specs:
+        if key in global_description:
+            site_specs[key] = site_specs[key]+'    ['+''.join(global_description[key])+']'
+            
+
+def site_specs(site: str, 
+               data_type: str) -> dict:
+    """Site specific global attributes and parameters"""
+
+    if site == 'juelich':
+        
+        params = {
+            'station_altitude': 108., # m MSL
+            'station_longitude': 6.407, # degrees east
+            'station_latitude': 50.906, # degrees north
+            
+            # integration time BLB files: 
+            # The integration time for a boundary layer scan (RPG-specific) must be specified here. 
+            # This is only relevant if BLB files for boundary layer temperature profiles are created. 
+            # You can find this number with the RPG software when you load your Measurement Definition File (MDF) and open "Definition of Measurement and Calibration Parameters" 
+            # --> "Products and Integration" --> "Total Integration Time" --> "Brightness Temperatures (BL). 
+            # Specify integration time in seconds.
+            'scan_time' : 50.,
+            
+            # integration time of measurements in seconds
+            'int_time': 1, 
+            
+            # Bandwidth of the central frequency in GHz (center frequency of single of upper side-band)
+            'bandwidth': np.array([230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 600, 1000, 2000]),
+            
+            # Single, double, or double-double sideband (1, 2, 4 are possible values)
+            'sideband_count': 1,
+            
+            # 56.xx +/- X +/- Y
+            'sideband_IF_separation': np.array([[0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.]]),
+            
+            # offset correction for TBs, i.e. adjustement of observation to nominal frequency:
+            # Note: RPG offers a frequency shift within the radiometer software. 
+            # This shift will modify the TBs calculated with the RPG software. 
+            # Original TBs CANNOT be reconstructed, unless you have recorded the voltages & calibration parameters and possess adequate software routines to perform a re-calibration.
+            # Hence, the authors of actris_mwr_pro DO NOT recommend to apply the frequency shifts in general. 
+            # If you have applied these frequency shifts, you may still give these to protocol so that they are contained in the resulting level1 netcdf files. 
+            # The variable freq_shift specifies the frequency shifts applied [in MHz]. 
+            # Set all to 0 if no frequency shifts were applied."""
+            'freq_shift' : np.array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]),
+            
+            # [lower, upper] bound threshold of TB quality flags in K
+            'TB_threshold': np.array([2.7, 330.]), 
+            
+            # solar angle flagging:
+            # saf parameter lets the user determine within what angular region (in deg) around the sun the MWR TBs and products are flagged, 
+            # e.g. saf = 5 means that all values with +-5 deg elevation and +-5 deg azimuth will be flagged. 
+            # Note that this flag only works if radiometer position is correctly specified and the radiometer is aligned towards the North.
+            'saf': 7.,
+            
+            # azimuth correction:
+            # Azimuth angle is transformed to geographical coordinates (E=90 and W=270), currently only for RPG scanners.
+            # Set az_cor to the angle that RPG software gives when instrument is pointing to the North.
+            # If you do not want to transform the coordinates set azi_cor to -999.
+            'azi_cor': 0.,
+            
+            # active quality flags for level 1 data; 1: flag active
+            # Bit 1: Missing TB-value\n'
+            # Bit 2: TB threshold (lower range)\n'
+            # Bit 3: TB threshold (upper range)\n'
+            # Bit 4: Spectral consistency threshold\n'
+            # Bit 5: Receiver sanity\n'
+            # Bit 6: Rain flag\n'
+            # Bit 7: Solar flag\n'
+            # Bit 8: TB offset threshold'
+            'flags_active': [1,1,1,0,1,1,1,0],
+            
+            # Retrieval coefficient files
+            # Specify retrieval algorithm parameter files to be used in retrieving Level 2 products here.
+            'algo_lwp': ['lwp_deb_rt00_90'],
+            'algo_iwv': ['iwv_deb_rt00_90'],
+        }
+
+        global_specs = {
+        'conventions' : 'CF-1.8',
+        'title' : 'Juelich RPG HATPRO G5',
+        'history' : '',
+        'institution' : 'University of Cologne',
+        'source' : 'Ground Based Remote Sensing',
+        'comment' : '',
+        'references' : '',
+        'site_location' : 'Juelich, Germany',
+        'instrument_id' : '',
+        'wigos_station_id' : '0-276-0-10508',
+        'principal_investigator' : 'Institute for Geophysics and Meteorology (IGMK)',
+        'instrument_manufacturer' : 'Radiometer Physics (RPG)',
+        'instrument_model' : 'RPG-HATPRO',
+        'instrument_generation' : 'G5',
+        'instrument_hw_id' : '',
+        'network_name' : 'ACTRIS',
+        'campaign_name' : '',
+        'dependencies' : '',
+        'license' : '',
+        'instrument_calibration_status' : '',
+        'date_of_last_absolute_calibration' : '',
+        'date_of_last_covariance_matrix' : '',
+        'type_of_automatic_calibrations_performed' : '',
+        'factory_history' : '',
+        'ir_instrument_manufacturer' : '',
+        'ir_instrument_model' : 'RPG-HATPRO',     
+        'ir_instrument_fabrication_year' : '',   
+        'met_instrument_manufacturer' : '',
+        'met_instrument_model' : '',    
+        'met_instrument_fabrication_year' : '',        
+        'air_temperature_accuracy' : '',
+        'relative_humidity_accuracy' : '',
+        'air_pressure_accuracy' : '',
+        'rain_rate_accuracy' : '',
+        'wind_direction_accuracy' : '',
+        'wind_speed_accuracy' : '',                  
+        # Level 2 specific:
+        'retrieval_type' : '',
+        'retrieval_elevation_angles' : '',
+        'retrieval_frequencies' : '',
+        'retrieval_auxiliary_input' : '',
+        'retrieval_description': '',
+        } 
+        
+
+    else:
+        raise RuntimeError(['Site '+ site +' not supported'])        
+        
+    if data_type == '1B01':
+        
+        values = list(global_specs.values())[0:24]
+        keys = list(global_specs.keys())[0:24]
+        specs = {k: v for k, v in zip(keys, values)}
+
+    elif data_type == '1B11':
+        
+        vv = list(global_specs.values())[:]
+        kk = list(global_specs.keys())[:]
+        keys = kk[0:11] + kk[24:26] + kk[15:17] + kk[18:19] + kk[23:24] + kk[26:27]
+        values = vv[0:11] + vv[24:26] + vv[15:17] + vv[18:19] + vv[23:24] + vv[26:27]
+        specs = {k: v for k, v in zip(keys, values)}
+
+    elif data_type == '1B21':
+        
+        vv = list(global_specs.values())[:]
+        kk = list(global_specs.keys())[:]
+        keys = kk[0:11] + kk[27:29] + kk[15:17] + kk[18:19] + kk[23:24] + kk[30:36] + kk[29:30]
+        values = vv[0:11] + vv[27:29] + vv[15:17] + vv[18:19] + vv[23:24] + vv[30:36] + vv[29:30]
+        specs = {k: v for k, v in zip(keys, values)}
+
+    elif data_type == '1C01':
+        
+        vv = list(global_specs.values())[:]
+        kk = list(global_specs.keys())[:]
+        keys = kk[0:36]
+        values = vv[0:36]
+        specs = {k: v for k, v in zip(keys, values)}
+        
+    elif data_type in ('2P00','2I00','2S00'):
+        
+        vv = list(global_specs.values())[:]
+        kk = list(global_specs.keys())[:]
+        keys = kk[0:11] + kk[37:41]
+        values = vv[0:11] + vv[37:41]
+        specs = {k: v for k, v in zip(keys, values)}        
+
+    else:
+        raise RuntimeError(['Data type '+ data_type +' not supported for file writing.'])
+
+    return specs, params
