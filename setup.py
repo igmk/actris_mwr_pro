a3ba49348833627175f03c056eabc3549cfaef4c
Antoine Gibek <antoine.gibek@ipsl.fr>
2023-02-28 18:26:22 +0100

    feat: Add package architecture with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,34 +1,10 @@
+"""The setup script."""
+from typing import Dict
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-version: dict = {}
-with open("mwrpy/version.py", encoding="utf8") as f:
-    exec(f.read(), version)  # pylint: disable=W0122
+version: Dict[str, str] = {}
+with open("mwrpy/version.py") as fp:
+    exec(fp.read(), version)
 
-with open("README.md", encoding="utf8") as f:
-    readme = f.read()
-
-setup(
-    name="mwrpy",
-    version=version["__version__"],
-    description="Python package for Microwave Radiometer processing in ACTRIS",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author="University of Cologne",
-    author_email="actris-ccres-mwr@uni-koeln.de",
-    url="https://github.com/igmk/actris_mwr_pro",
-    packages=find_packages(),
-    include_package_data=True,
-    python_requires=">=3.10",
-    install_requires=[
-        "scipy",
-        "netCDF4",
-        "matplotlib",
-        "metpy",
-        "ephem",
-        "pandas",
-    ],
-    extras_require={
-        "test": ["pylint", "mypy"],
-    },
-)
+setup(version=version["version"])
